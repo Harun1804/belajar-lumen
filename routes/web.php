175bccc24e4ee0cedaf32a3818fb1a808530faa4
Harun1804 <Harun.arrasyid1804@gmail.com>
2022-06-14 13:45:11 +0700

    Add Cors Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 /*
@@ -15,6 +17,9 @@
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+$router->get('/key',function() {
+    return Str::random(32);
 });
 
 $router->post('/login', 'AuthController@login');
